8822409381e4d7bdb8563bb58f57727951c49c8e
Francis <francispham89@gmail.com>
2019-03-06 21:38:43 -0800

    Implementing Adding the Request Driven Login Solution & Setting Cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,18 @@
 exports.getLogin = (req, res, next) => {
+    // console.log(req.get('Cookie'));                       
+    const isLoggedIn = req
+        .get('Cookie')
+        .trim()
+        .split('=')[1];
+    // console.log(isLoggedIn);                       
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: isLoggedIn
     });
+};
+
+exports.postLogin = (req, res, next) => {
+    res.setHeader('Set-Cookie', 'loggedIn=true');
+    res.redirect('/');
 };
