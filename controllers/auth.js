b6dd467dfab1cd3b955b9a96c217e9723259db50
Francis <francispham89@gmail.com>
2019-03-16 16:16:21 -0700

    Implementing Signin Functionality in Auth Controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -29,15 +29,30 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    User.findById('5c7ae7918ccd1c329cd986a2')
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
         .then(user => {
-            req.session.isLoggedIn = true;
-            req.session.user = user;
-            // When you need the session surely save in the database
-            req.session.save(err => {
-                console.log(err);
-                res.redirect('/');
-            });
+            if(!user) {
+                return res.redirect('/login');
+            }
+            bcrypt.compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isLoggedIn = true;
+                        req.session.user = user;
+                        // When you need the session surely save in the database
+                        return req.session.save(err => {
+                            console.log(err);
+                            res.redirect('/');
+                        });
+                    }
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    console.log(err); 
+                    res.redirect('/login')
+                });
         })
         .catch(err => console.log(err));
 };
