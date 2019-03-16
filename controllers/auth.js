457fc569d1464ef267bc5c486a4b8b16e4f80bdf
Francis <francispham89@gmail.com>
2019-03-15 20:19:51 -0700

    Intsalling bcryptjs & Implementing Encrypting Passwords

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -49,12 +51,15 @@ exports.postSignup = (req, res, next) => {
             if (userDoc) {
                 return res.redirect('/signup');
             }
+            return bcrypt.hash(password, 12);
+        })
+        .then(hashedPassword => {
             const user = new User({
                 email: email,
-                password: password,
+                password: hashedPassword,
                 cart: { items: [] }
             });
-            return user.save();
+            return user.save();    
         })
         .then(result => {
             console.log(result);
