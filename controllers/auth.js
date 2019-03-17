01b14d63aceab4eeff136eaed7db5fd0341e9ba9
Francis <francispham89@gmail.com>
2019-03-17 13:49:51 -0700

    Implementing Flash Messages for SignUp

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,9 +26,16 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+        message = message[0];
+    } else {
+        message = null
+    }
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
+        errorMessage: message
     })
 };
 
@@ -38,7 +45,10 @@ exports.postLogin = (req, res, next) => {
     User.findOne({ email: email })
         .then(user => {
             if(!user) {
-                req.flash('error', 'Invalid email or password. :)');
+                req.flash(
+                    'error', 
+                    'Invalid email or password. :)'
+                );
                 return res.redirect('/login');
             }
             bcrypt.compare(password, user.password)
@@ -52,6 +62,10 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
+                    req.flash(
+                        'error', 
+                        'Invalid email or password. :)'
+                        );
                     res.redirect('/login');
                 })
                 .catch(err => {
@@ -69,6 +83,10 @@ exports.postSignup = (req, res, next) => {
     User.findOne({ email: email })
         .then(userDoc => {
             if (userDoc) {
+                req.flash(
+                  "error",
+                  "Email exists already, please pick a different one. :)"
+                );
                 return res.redirect('/signup');
             }
             return bcrypt.hash(password, 12)
