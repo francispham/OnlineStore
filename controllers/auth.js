521bbfc05281c1498759f074721e87a707ce168d
Francis <francispham89@gmail.com>
2019-04-03 22:49:03 -0700

    Implementing Validation to Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -32,7 +32,12 @@ exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: message
+        errorMessage: message,
+        oldInput: {
+            email: '',
+            password: ''
+        },
+        validationErrors: []
         // isAuthenticated: isLoggedIn // Before Session
     });
 };
@@ -66,17 +71,27 @@ exports.postLogin = (req, res, next) => {
         return res.status(422).render('auth/login', {
             path:'/login',
             pageTitle: 'Login',
-            errorMessage: errors.array()[0].msg
+            errorMessage: errors.array()[0].msg,
+            oldInput: {
+                email: email,
+                password: password
+            },
+            validationErrors: errors.array()
         });
     }
     User.findOne({ email: email })
         .then(user => {
             if(!user) {
-                req.flash(
-                    'error', 
-                    'Invalid email or password. :)'
-                );
-                return res.redirect('/login');
+                return res.status(422).render('auth/login', {
+                    path: '/login',
+                    pageTitle: 'Login',
+                    errorMessage: 'Invalid email or password.',
+                    oldInput: {
+                        email: email,
+                        password: password
+                    },
+                    validationErrors: []
+                });
             }
             bcrypt.compare(password, user.password)
                 .then(doMatch => {
@@ -89,11 +104,16 @@ exports.postLogin = (req, res, next) => {
                             res.redirect('/');
                         });
                     }
-                    req.flash(
-                        'error', 
-                        'Invalid email or password. :)'
-                        );
-                    res.redirect('/login');
+                    return res.status(422).render('auth/login', {
+                        path: '/login',
+                        pageTitle: 'Login',
+                        errorMessage: 'Invalid email or password.',
+                        oldInput: {
+                            email: email,
+                            password: password
+                        },
+                        validationErrors: []
+                    });
                 })
                 .catch(err => {
                     console.log(err); 
