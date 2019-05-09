9e1c4d347f8939aedbc3da2f2042c04c5644a9d9
Francis <francispham89@gmail.com>
2019-05-08 17:18:07 -0700

    Implementing Pagination in Product List Page

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,13 +9,29 @@ const PDFDocument = require('pdfkit');
 const ITEMS_PER_PAGE = 1; //For Implementing Pagination (FIP)
 
 exports.getProducts = (req, res, next) => {
+    const page = +req.query.page || 1; // FIP
+    let totalItems;
+
     Product.find()
+        .countDocuments()
+        .then(numProducts => {
+            totalItems = numProducts;
+            //For Implementing Pagination:
+            return Product.find()
+                .skip((page - 1) * ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE);
+        })
         .then(products => {
-            console.log(products);
             res.render('shop/product-list', {
                 prods: products,
-                pageTitle: 'All Products',
-                path: '/products'
+                pageTitle: 'Products',
+                path: '/products',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage: page > 1,
+                nextPage: page + 1,
+                previousPage: page - 1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
