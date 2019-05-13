adc0568e9cfc2e9885e08868b2691b16a6ba4ba9
Francis <francispham89@gmail.com>
2019-05-13 14:16:45 -0700

    Implementing Async Requests Step6: Manipulating the DOM

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,6 +3,8 @@ const deleteProduct = (btn) => {
     const prodId = btn.parentNode.querySelector('[name=productId').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf').value;
 
+    const productElement = btn.closest('article');
+
     fetch('/admin/product/' + prodId, {
         method: 'DELETE',
         headers: {
@@ -11,6 +13,11 @@ const deleteProduct = (btn) => {
     })
     .then(result => {
         console.log(result);
+        return result.json();
+    })
+    .then(data => {
+        console.log(data);
+        productElement.parentNode.removeChild(productElement);
     })
     .catch(err => {
         console.log(err);
