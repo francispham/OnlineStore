321cdf0c14f6a3ee608da3cfba1a6d5c8ba53c66
Francis <francispham89@gmail.com>
2019-05-15 16:03:56 -0700

    Implementing Payment Feature Part1: Creating Checkout Controler & Route

--- controllers/shop.js
+++ controllers/shop.js
@@ -143,6 +143,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
         });
 };
 
+exports.getCheckout = (req, res, next) => {
+    req.user
+        .populate('cart.items.productId')
+        .execPopulate()
+        .then(user => {
+            // console.log(user.cart.items);
+            const products = user.cart.items;
+            let total = 0;
+            products.forEach(p => {
+                total += p.quantity * p.productId.price;
+            });
+            res.render('shop/checkout', {
+                path: '/checkout',
+                pageTitle: 'Checkout',
+                products: products,
+                totalSum: total 
+            });
+        })
+        .catch(err => {
+            const error = new Error(err);
+            error.httpStatusCode = 500;
+            return next(error);
+        });
+}
+
 exports.postOrder = (req, res, next) => {
     req.user
         .populate('cart.items.productId')
